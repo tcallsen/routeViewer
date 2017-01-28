87c8003402734324d574bf9b6352073783b06ced
Taylor Callsen <gritto2008@gmail.com>
2017-01-28 11:05:58 -0600

    forgot to include server webpack config

---
+++ server/webpack.dev.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	entry: [
+		'../app/src/index.js'
+	],
+	output: {
+		path: '/',
+		publicPath: 'http://localhost:3000/build/',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: './build',
+		hot: true
+	}
+};
