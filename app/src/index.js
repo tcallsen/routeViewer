5b1b7a8777b1bf56d169690649e505587881dab8
Taylor Callsen <gritto2008@gmail.com>
2017-01-29 12:52:50 -0600

    updated front end to draw and center on new routes pushed forward

--- app/src/index.js
+++ app/src/index.js
@@ -8,30 +8,36 @@ var io = require('socket.io-client');
 var socket = io();
 socket.on('connect', function(){
 	console.log('socket connceted');
-	/*
-	console.log('sending message to server');
-	socket.emit('handshake', 'browser here');
-	socket.on('handshake', () => socket.send('good day'));
-	*/
 });
+
 socket.on('newRoutes', function(newRoutes){
-	console.log('newRoutes', newRoutes.length);
+	
+	console.log('newRoutes received:', newRoutes.length);
+
+	var geoJsonParser = new ol.format.GeoJSON(); //{ featureProjection: 'EPSG:4326' }
+	var routesLayerSource = routesLayer.getSource();
+
+	newRoutes.forEach( route => {
+		var routeFeature = geoJsonParser.readFeature( route );
+		routesLayerSource.addFeature( routeFeature ); 
+	}); 
+
+	if (map) map.getView().fit( routesLayerSource.getExtent(), map.getSize() );
+
 });
 
 // OPEN LAYERS map
+
+var routesLayer = new ol.layer.Vector({
+	source: new ol.source.Vector({
+		features:[],
+		wrapX: false
+	}),
+});
+
 var map = new ol.Map({
 	target: 'app',
 	layers: [
-		/* new ol.layer.Tile({
-			source: new ol.source.XYZ({
-				url: 'https://b.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidGRjZG9kZ2VyIiwiYSI6ImNpeWhoemhpMTA1cWgyd212enVrb2lrbGIifQ.ujdf9NaM8L8sYTDP1dWvMw'
-			})
-		}) */
-		/* new ol.layer.Tile({
-            source: new ol.source.Stamen({
-            	layer: 'terrain'
-            })
-      	}) */
       	new ol.layer.Tile({
 	        source: new ol.source.OSM({
 	            url: 'http://mt{0-3}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
@@ -40,7 +46,8 @@ var map = new ol.Map({
 	                new ol.Attribution({ html: '<a href="https://developers.google.com/maps/terms">Terms of Use.</a>' })
 	            ]
 	        })
-	    })
+	    }),
+	    routesLayer
 	],
 	view: new ol.View({
 		projection: 'EPSG:4326',
